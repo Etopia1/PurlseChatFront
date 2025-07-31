727d2cb25bfd0801e0c6f5c6bfa2982ffcd7a880
Etopia1 <jolaetopia81@gmail.com>
2025-07-31 03:01:46 +0100

    new commit

--- src/lib/api.axios.js
+++ src/lib/api.axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: "https://pulsechatback.onrender.com/api",
+    baseURL: "https://purslsechatback.onrender.com/api",
     withCredentials: true
 }) 
